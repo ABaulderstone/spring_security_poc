ec2e7948c486032dbc7c60812050d62d42554f88
ABaulderstone <alex.baulderstone@gmail.com>
2025-06-17 10:50:57 +0930

    add service annotation to userdetailsservice

--- src/main/java/com/example/securitypoc/auth/UserDetailsServiceImpl.java
+++ src/main/java/com/example/securitypoc/auth/UserDetailsServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.securitypoc.auth;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.example.securitypoc.user.UserRepository;
 import com.example.securitypoc.user.entities.User;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
