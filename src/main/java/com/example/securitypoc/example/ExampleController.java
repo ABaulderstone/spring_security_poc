66ea16015500120e7cfce6ffd0f08d2c4f66a144
ABaulderstone <alex.baulderstone@gmail.com>
2025-06-17 18:29:47 +0930

    csrf protection works for one request only

--- src/main/java/com/example/securitypoc/example/ExampleController.java
+++ src/main/java/com/example/securitypoc/example/ExampleController.java
@@ -3,6 +3,7 @@ package com.example.securitypoc.example;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @RestController
 @RequestMapping("/example")
@@ -11,6 +12,11 @@ public class ExampleController {
     @GetMapping("/protected-route")
     public String getMethodName() {
         return "You can only access this with the right credentials";
+    }
+
+    @PostMapping("/protected-route")
+    public String postMethodName() {
+        return "This should be protected by CSRF";
     }
 
 }
