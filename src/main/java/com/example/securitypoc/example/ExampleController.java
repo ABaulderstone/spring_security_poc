2ae6d098d954e9b3605c7ea073474471c3bda1bb
ABaulderstone <alex.baulderstone@gmail.com>
2025-06-19 11:02:31 +0930

    updates AllowedRoles to use enum

--- src/main/java/com/example/securitypoc/example/ExampleController.java
+++ src/main/java/com/example/securitypoc/example/ExampleController.java
@@ -2,8 +2,14 @@ package com.example.securitypoc.example;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.securitypoc.auth.role.AllowedRoles;
+import com.example.securitypoc.auth.role.Role;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/example")
@@ -17,6 +23,12 @@ public class ExampleController {
     @PostMapping("/protected-route")
     public String postMethodName() {
         return "This should be protected by CSRF";
+    }
+
+    @AllowedRoles({ Role.ADMIN })
+    @GetMapping("/admin-only")
+    public String getMethodName(@RequestParam String param) {
+        return "Only an admin can see this";
     }
 
 }
