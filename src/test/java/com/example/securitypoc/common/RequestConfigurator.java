c391b21d49cbd7aff3c0e2082b49ca4b44444851
ABaulderstone <alex.baulderstone@gmail.com>
2025-08-17 21:15:49 +0930

    update configurator

--- src/test/java/com/example/securitypoc/common/RequestConfigurator.java
+++ src/test/java/com/example/securitypoc/common/RequestConfigurator.java
@@ -7,7 +7,7 @@ import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSender;
+
 import io.restassured.specification.RequestSpecification;
 
 public class RequestConfigurator {
